b654c029a1e01fd03d31c668bb5f9acb4e8da758
EvilBruce <wdw881@gmail.com>
2016-07-26 21:31:29 -0700

    Second commit

---
+++ app/src/main/java/com/sw/smallworld/FeedItem.java
@@ -0,0 +1,56 @@
+package com.sw.smallworld;
+
+/**
+ * Created by wdw88_000 on 7/26/2016.
+ */
+public class FeedItem {
+    String  title;
+    String  link;
+    String  description;
+    String  pubDate;
+    String  thumbnailUrl;
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPubDate() {
+        return pubDate;
+    }
+
+    public void setPubDate(String pubDate) {
+        this.pubDate = pubDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
+    }
+
+    public void setThumbnailUrl(String thumbnailUrl) {
+        this.thumbnailUrl = thumbnailUrl;
+    }
+
+
+
+
+}
--- app/src/main/java/com/sw/smallworld/MainActivity.java
+++ app/src/main/java/com/sw/smallworld/MainActivity.java
@@ -2,12 +2,16 @@ package com.sw.smallworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
-
+    RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        ReadRss readRss = new ReadRss(this,recyclerView);
+        readRss.execute();
     }
 }
